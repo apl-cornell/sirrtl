10c1495fd8e8e82a854b815372b0d67b9e681de6
Andrew Ferraiuolo <af433@cornell.edu>
2017-11-20 20:34:48 -0500

    Bug Fix to ApplyVecLabels pass

--- src/main/scala/firrtl/passes/labelchecking/ApplyVecLabels.scala
+++ src/main/scala/firrtl/passes/labelchecking/ApplyVecLabels.scala
@@ -8,6 +8,7 @@ import firrtl.Utils._
 object ApplyVecLabels extends Pass with PassDebug {
   override def name = "Apply indexes to vector labels"
   override def debugThisPass = false
+  val bot = ProdLabel(PolicyHolder.bottom, PolicyHolder.top)
 
   def apply_index_vech(l: Label, idx: Expression): Label = {
     def apply_index_c(idx: Expression)(lc: LabelComp): LabelComp = 
@@ -20,16 +21,30 @@ object ApplyVecLabels extends Pass with PassDebug {
     apply_index_(idx)(l)
   }
   
+  def apply_index_vech(l: Label, idx: Int): Label = {
+    def apply_index_c(idx: Int)(lc: LabelComp): LabelComp = 
+      lc map apply_index_c(idx) match {
+        case lcx: VecHLevel => 
+          val idx_ = UIntLiteral(idx, vec_size(lcx.arr.tpe), bot)
+          IndexedVecHLevel(lcx.arr, idx_)
+        case lcx => lcx
+      }
+    def apply_index_(idx: Int)(l: Label): Label = 
+      l map apply_index_(idx) map apply_index_c(idx)
+    apply_index_(idx)(l)
+  }
+  
   def apply_vec_labels_e(e: Expression) : Expression =
     e map apply_vec_labels_e match {
       case ex: WSubIndex => 
         dprintb(s"subIndex: ${ex.serialize}")
         dprintb(ex.toString)
-        val lbl_ = apply_index_vech(ex.exp.lbl, uint(ex.value))
+        val lbl_ = apply_index_vech(ex.exp.lbl, ex.value)
         ex copy (lbl = lbl_)
       case ex: WSubAccess => 
         dprintb(s"subAccess: ${ex.serialize}")
         dprintb(ex.toString)
+        // AF: I suspect the bit-width of ex.index will cause problems in z3
         val lbl_ = apply_index_vech(ex.exp.lbl, ex.index)
         ex copy (lbl = JoinLabel(lbl_, ex.index.lbl))
       case ex => ex
@@ -40,6 +55,10 @@ object ApplyVecLabels extends Pass with PassDebug {
 
   def apply_vec_labels(m: DefModule): DefModule = {
     m map apply_vec_labels_s
+  }
+
+  def vec_size(t: Type) = t match {
+    case tx: VectorType => IntWidth(log2Up(tx.size))
   }
 
   def run(c: Circuit) = {
